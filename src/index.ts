20c60be037f4c9c047bfa00eb47aa8b03fe74258
Bogdanov Anton <51376798+Bogdanov-am@users.noreply.github.com>
2022-02-12 13:39:27 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { TestServer } from './core/app/app'
-import { ObjectId } from 'mongodb'
 
 const app = new TestServer()
 app.init()
 
-
 process.on('SIGINT', () => app.finally())
+process.on('SIGTERM', () => app.finally())
